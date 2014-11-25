732fc7b0b28179190c5cb4375306e1d35e233936
Chris MacNaughton <chmacnaughton@gmail.com>
2014-11-25 16:13:49 +0000

    add bookmarklet link

--- mapping.rb
+++ mapping.rb
@@ -14,8 +14,9 @@ before do
 end
 
 get '/' do
-  puts 'hello'
-  binding.pry
+  bookmarklet = 'javascript:((function()%7Bvar%20t,e,s,i,n%0At=function()%7Bfunction%20t(t,e,s)%7Bfor(this.id=t,this.script=e,this.style=s;null!==this.find(this.id);)this.remove(this.find(this.id))%0Anull===this.find(this.id)&&this.add()%7Dreturn%20t.prototype.find=function()%7Breturn%20document.getElementById(this.id)%7D,t.prototype.remove=function()%7Breturn%20document.body.removeChild(this.find(this.id))%7D,t.prototype.add=function()%7Bvar%20t,e,s%0Areturn%20t=document.createElement(%22div%22),t.id=this.id,t.setAttribute(%22data-auth-token%22,%22saYov45aJve4LP6cqqKi%22),null!==this.script&&(e=document.createElement(%22script%22),e.type=%22text/javascript%22,e.charset=%22UTF-8%22,e.src=this.script,t.appendChild(e)),null!==this.style&&(s=document.createElement(%22link%22),s.href=this.style,s.rel=%22stylesheet%22,s.type=%22text/css%22,t.appendChild(s)),document.body.appendChild(t)%7D,t%7D(),s=%221.8.0%22,null==window.jQuery%7C%7Cwindow.jQuery.fn.jquery%3Cs?(e=!1,n=document.createElement(%22script%22),n.src=%22http://ajax.googleapis.com/ajax/libs/jquery/%22+s+%22/jquery.min.js%22,n.onload=n.onreadystatechange=function()%7Bvar%20s%0Areturn%20e%7C%7Cthis.readyState&&%22loaded%22!==this.readyState&&%22complete%22!==this.readyState?void%200:(e=!0,s=new%20t(%22worthit%22,%22http://localhost:4567/assets/bookmarklet.js%22,%22http://localhost:4567/assets/bookmarklet.css%22))%7D,document.getElementsByTagName(%22head%22)[0].appendChild(n)):i=new%20t(%22worthit%22,%22http://localhost:4567/assets/bookmarklet.js%22,%22http://localhost:4567/assets/bookmarklet.css%22)%7D).call(this)%0A);'
+  content_type :html
+  "Drag this link to your bookmarklet bar to develop on the WorthIt Mappings: <a href='#{bookmarklet}''>WorthItDev</a>"
 end
 
 get '/api/mappings/*' do
